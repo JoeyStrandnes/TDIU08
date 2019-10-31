b8b332d23568453f38718ae549ca0692918a6a23
josst471 <joey.makdesi1@hotmail.com>
2019-10-31 15:47:32 +0100

    Lab_0: First

---
+++ C++/Lab_0/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+int Hel;
+double Decimal;
+float Flyt;
+
+
+
+int main(void) {
+
+  std::cout << "Skriv in ett heltal: ";
+  std::cin >> Hel;
+  std::cout << "Du skrev in ett heltal: " << (Hel) << '\n';
+
+  std::cout << "Skriv in ett flyttal:";
+  std::cin >> Flyt;
+  std::cout << "Du skrev in ett flyttal: " << (Flyt) << '\n';
+
+  std::cout << "Skriv in ett heltal och ett flyttal: ";
+  std::cin >> Hel;
+  std::cin >> Flyt;
+  std::cout << "Du skrev in heltalet: " << (Hel) <<'\n';
+  std::cout << "Du skrev in flyttalet: " << (Flyt) <<'\n';
+
+  std::cout << "Skriv in ett heltal och ett flyttal: " << '\n';
+
+
+
+
+
+  while (1) {
+    /* code */
+  }
+}
